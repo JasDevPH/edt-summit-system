93f80e08aaa5885d63d18661c446b599d2de4824
JasDevPH <jasdevph@gmail.com>
2026-03-14 10:50:37 +0800

    dsd

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: "#f5f3ff",
     icons: [
       {
-        src: "/icons/icon-192x192.png",
+        src: "/edt logo.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icons/icon-512x512.png",
+        src: "/edt logo.png",
         sizes: "512x512",
         type: "image/png",
       },
